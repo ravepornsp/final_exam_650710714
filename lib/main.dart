48432b4a88843d73db75519e6f2a66781429f9d9
ravepornsp <supapanchai_r@su.ac.th>
2025-03-15 11:21:24 +0700

    ready_for_exam

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/profile',
+      routes: {
+        '/profile': (context) => const Profile(),
+      },
+    );
+  }
+}
---
+++ lib/profile.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Card(
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset(
+                    "asset/profile.jpg",
+                    width: 400,
+                    height: 400,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Raveporn Supapanchai",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5),  
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.book, // ใช้ไอคอนโปรไฟล์ที่ถูกต้อง
+                      size: 30,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "Silpakorn University",
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.phone, // ใช้ไอคอนโปรไฟล์ที่ถูกต้อง
+                      size: 30,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "098-378-7681",
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.email, // ใช้ไอคอนโปรไฟล์ที่ถูกต้อง
+                      size: 30,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "supapanchai_r@silpakorn.edu",
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
